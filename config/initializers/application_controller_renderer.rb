fea17efe4c04b68370e5a12db55b562f81fb91c7
きむらあやか <ha.iy.r8ao@gmail.comgit config --global user.name chorin11>
2020-07-13 20:03:05 +0900

    [Add]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'c3aba2c75930c9ce7cef292531cca64a5f47505417f56a1b7a7281fd0985016bdd8c6fa494b5d1c7f866a79f3b4336f34f9903a82bc45152f7337b41fa5d4bca'
